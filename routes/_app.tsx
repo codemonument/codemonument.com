1aa28a89ed708656fd3f4b3193bb713168adb4a0
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-21 06:46:44 +0200

    add _app.tsx and global css file

---
+++ routes/_app.tsx
@@ -0,0 +1,18 @@
+import { asset, Head } from "$fresh/runtime.ts";
+import { AppProps } from "$fresh/src/server/types.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <html>
+      <Head>
+        <title>Fresh</title>
+        <link rel="stylesheet" href={asset("global.css")} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      </Head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
+}
