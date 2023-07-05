30e2b044c23b4cbb55e6d9e47119b8e51c4c4b8c
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-07-05 16:57:30 +0200

    upgrade fresh

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,23 +2,25 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $1 from "./routes/_app.tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/postcss/[...path].ts";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/_app.tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/postcss/[...path].ts": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
