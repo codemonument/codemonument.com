7864d67b8385d19990c358281efaf17d9ac7ad0c
Benjamin Jesuiter <bjesuiter@gmail.com>
2026-06-21 13:39:50 +0200

    wip: preserve codemonument site changes
    Preserves local site changes before Mac restore. Review before continued work.

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,15 @@
 import Layout from '@/components/Layout.tsx';
+// import 'iconify-icon';
+// import {Icon} from '@iconify-icon/react';
+import {Icon} from "@iconify/react"w
 
 export default function Home() {
 	return (
 		<Layout>
-			<div class="p-4 mx-auto max-w-screen-md"></div>
+			<div class="p-4 mx-auto max-w-screen-md">
+				<Icon icon="mdi-light:home" />
+				{/* <iconify-icon icon="mdi:home"></iconify-icon> */}
+			</div>
 		</Layout>
 	);
 }
